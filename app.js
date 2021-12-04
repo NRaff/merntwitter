7f1f0edc9080292b2c1b52bd20d8741527fd02b4
nraff <nraff510@gmail.com>
2021-12-04 15:18:32 -0800

    complete auth routes

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose')
 const express = require("express");
-// const mongoose = require('mongoose');
-// import mongoURI from "./config/keys";
-const mongoURI = require("./config/keys")
+const Keys = require("./config/keys")
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 mongoose
-  .connect(mongoURI, {useNewUrlParser: true})
+  .connect(Keys.mongoURI, {useNewUrlParser: true})
   .then(() => console.log("Connect to MongoDB successfully"))
   .catch(err => console.log(err))
 
 const port = process.env.PORT || 5000;
 const app = express()
+app.use(passport.initialize())
+require('./config/passport')(passport)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
 app.get("/", (req, res) => res.send("Hello all the things"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
