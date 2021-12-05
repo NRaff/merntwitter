88f1203612f210087ecf2f4beb0290f2990510ef
nraff <nraff510@gmail.com>
2021-12-05 15:13:11 -0800

    prepare deploy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const users = require("./routes/api/users")
 const tweets = require("./routes/api/tweets")
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 mongoose
   .connect(Keys.mongoURI, {useNewUrlParser: true})
@@ -15,6 +16,12 @@ const port = process.env.PORT || 5000;
 const app = express()
 app.use(passport.initialize())
 require('./config/passport')(passport)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use("/api/users", users)
